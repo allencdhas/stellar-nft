c9bde160c4940307869c38274a24641579233f0f
Allen Chrisso Dhas <allencdvja@gmail.com>
2023-12-05 01:43:25 +0530

    deployment openzeppelincontracts version update

--- contracts/StellarNFT.sol
+++ contracts/StellarNFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -9,32 +9,33 @@ import "hardhat/console.sol";
 
 contract StellarNFT is ERC721 {
     using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+    Counters.Counter private _tokenIds;
 
-  // We need to pass the name of our NFTs token and its symbol.
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
-    console.log("This is my NFT contract. Woah!");
-  }
+    // We need to pass the name of our NFTs token and its symbol.
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract. Woah!");
+    }
 
-  // A function our user will hit to get their NFT.
-  function makeAnEpicNFT() public {
-     // Get the current tokenId, this starts at 0.
-    uint256 newItemId = _tokenIds.current();
+    // A function our user will hit to get their NFT.
+    function makeAnNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
 
-     // Actually mint the NFT to the sender using msg.sender.
-    _safeMint(msg.sender, newItemId);
-    
-    // Return the NFT's metadata
-    tokenURI(newItemId);
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
 
-    // Increment the counter for when the next NFT is minted.
-    _tokenIds.increment();
-  }
+        // Return the NFT's metadata
+        tokenURI(newItemId);
 
-  // Set the NFT's metadata
-  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-    require(_exists(_tokenId));
-    return "blah";
-  }
-}
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+    }
+
+    // Set the NFT's metadata
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        require(_exists(_tokenId));
+        return "blah";
+    }
 }
